caf990c2fc004a049386fbebbc4acc69ed2ce340
m-amirhoseini <amirhoseini@danapardaz.com>
2025-10-25 00:48:02 +0330

    commit

--- DanaBet/DBModel/LeagueUsers.cs
+++ DanaBet/DBModel/LeagueUsers.cs
@@ -1,20 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
-namespace DanaBet.DBModel
+namespace DanaBet.DBModel;
+public class LeagueUser
 {
-    public class LeagueUser
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        [Required(ErrorMessage = "وارد کردن نام الزامی است")]
-        public string Firstname { get; set; } = string.Empty; // <<<< تغییر کلیدی: حذف 'required' و افزودن مقدار اولیه
+    [Required(ErrorMessage = "نام اجباری است")]
+    public string Firstname { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "وارد کردن نام خانوادگی الزامی است")]
-        public string Lastname { get; set; } = string.Empty; // <<<< تغییر کلیدی: حذف 'required' و افزودن مقدار اولیه
+    [Required(ErrorMessage = "نام خانوادگی اجباری است")]
+    public string Lastname { get; set; } = string.Empty;
 
-        public string? Mobile { get; set; }
+    [Required(ErrorMessage = "موبایل اجباری است")]
+    public string Mobile { get; set; } = string.Empty;
 
-        public int LeagueId { get; set; }
-        public virtual League? League { get; set; }
-    }
+    public int LeagueId { get; set; }
 }
